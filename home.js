4cf756c8468c094ba5a0caaafb4a2dd64fd7c823
Slim-Kasraoui <cabinet.kesraoui@hotmail.com>
2020-07-15 20:47:53 +0100

    starting

---
+++ home.js
@@ -0,0 +1,2 @@
+$('nav').css({'background-color': 'white','color':'black', 'text-decoration':'none', 'padding': '45px', 'margin' : '0px'});
+$('body').css({"background-color":"white", "margin": "0"})
