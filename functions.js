021f21400b522cc6fce01b02530ad98b44bf1b76
Slim-Kasraoui <cabinet.kesraoui@hotmail.com>
2020-07-17 02:33:30 +0100

    func and structure done

--- functions.js
+++ functions.js
@@ -2,31 +2,32 @@ function Products(image, description, price){
 	this.image = image;
 	this.description = description;
 	this.price = price;
+	
 };
 
-Products.prototype.getPrice = function() {
-	return this.price;
+Products.prototype.getPrice = function(category) {	
+	 return this.price;
 };
 
-var productsMen = [
-new Products('img', 'maryoul', 25),
-new Products('img', 'men shirt', 25),
-new Products('img', '5al3a', 25),
-new Products('img', 'men shirt', 25),
+var productsMen = [ 
+new Products('img', 'maryoul', 25 ),
+new Products('img', 'men shirt', 25 ),
+new Products('img', '5al3a', 25 ),
+new Products('img', 'men shirt', 25 ),
 ];
 
 var productsWomen = [
-new Products('img', 'dress', 25),
-new Products('img', 'dress', 25),
-new Products('img', 'dress', 25),
-new Products('img', 'dress', 25),
+new Products('img', 'dress', 25 ),
+new Products('img', 'dress', 30 ),
+new Products('img', 'dress', 14 ),
+new Products('img', 'dress', 5 ),
 ];
 
 var productsChildren = [
-new Products('img', 'child blue', 25),
-new Products('img', 'child green', 25),
-new Products('img', 'child rose', 25),
-new Products('img', 'child jean', 25),
+new Products('img', 'child blue', 25 ),
+new Products('img', 'child green', 25 ),
+new Products('img', 'child rose', 25 ),
+new Products('img', 'child jean', 25 ),
 ];
 
 
@@ -43,13 +44,34 @@ function generate(category){
 		var descrip = document.getElementById('desc' + i);
 		descrip.innerHTML = category[i].description;
 		var prix = document.getElementById('price' + i);
-		prix.innerHTML = category[i].price;
-	};
-
-	
+		prix.innerHTML = category[i].price;	
+		check("btn"+i, category[i].price)
+	};	
 };
 
 
+function check(id, selected){
+	var button = document.getElementById(id);
+
+	button.onclick = function(){
+		total = total + selected;
+		document.getElementById('output').innerHTML = total;
+	};
+}
+
+function loadBasket(){
+	location.href = 'basket.html';
+};
+
+document.getElementById("home").onclick = function () {
+        location.href = "home.html";
+    };
 
 
+var total = 0
 
+function sum(num) {
+      var price = document.getElementByClass("tot").innerHTML;
+      var y = parseInt(price) + parseInt(num)
+      document.getElementById("p1").innerHTML = y
+}
